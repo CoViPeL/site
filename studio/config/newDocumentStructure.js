c2fa03431a2468795d2db8fa9633cbe14c1e01ef
harshinde <lmfphks8@gmail.com>
2026-02-14 17:26:08 +0530

    Remove studio folder - static site only

--- studio/config/newDocumentStructure.js
+++
@@ -1,5 +0,0 @@
-// Only needed if we're importing schemas
-// import S from '@sanity/base/structure-builder';
-
-// Don't allow anyone to create new Homepage or Settings documents
-export default [];
--- studio/config/resolveDocumentActions.js
+++
@@ -1,5 +0,0 @@
-import defaultResolve from 'part:@sanity/base/document-actions';
-
-export default function resolveDocumentActions(props) {
-  return [...defaultResolve(props).filter((action) => action.name !== 'DeleteAction')];
-}
